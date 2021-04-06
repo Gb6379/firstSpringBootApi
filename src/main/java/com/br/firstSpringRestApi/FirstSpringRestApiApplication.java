658000d4c02a68f287d40f2a1626adeb0f9cc1ab
Gb6379 <gabriel.am.rosa@gmail.com>
2021-04-06 14:22:48 -0300

    first structure

---
+++ src/main/java/com/br/firstSpringRestApi/FirstSpringRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.br.firstSpringRestApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringRestApiApplication.class, args);
+	}
+
+}
